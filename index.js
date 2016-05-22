573954bb4ff639b3330e64bbdf8a444ffd2da997
Nicholas Asimov <kolyaazimov@gmail.com>
2016-05-22 08:16:35 +0300

    Working implementation

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').load();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const router = require('./router');
+
+const app = express();
+
+mongoose.connection.on('error', err => {
+  console.log('Could not connect to MongoDB server!');
+  console.error(err);
+});
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+router(app);
+
+const port = process.env.PORT || 3090;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}..`);
+});
