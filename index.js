05d0268802a000c83a6e91f87a2e2093de6b8e15
Nicholas Asimov <kolyaazimov@gmail.com>
2016-05-25 01:55:57 +0300

    Enabled CORS

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();
 
@@ -15,6 +16,7 @@ mongoose.connection.on('error', err => {
 mongoose.connect(process.env.MONGODB_URI);
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 
 router(app);
